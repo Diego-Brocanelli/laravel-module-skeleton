8a6981747c3b5e1f6ad405942afcefa3256c1c4f
Ricardo Pereira <contato@ricardopdias.com.br>
2020-05-22 19:29:28 -0300

    Adicionadas configurações padrões para o módulo

--- src/Config/skeleton.php
+++ src/Config/skeleton.php
@@ -2,7 +2,13 @@
 
 return [
 
-    // Para invocar essa configuração, deve-se usar config('skeleton.name')
-    'name' => 'skeleton'
+    // Para invocar essa configuração, deve-se usar config('skeleton.module_name')
+    'module_name' => 'skeleton',
+
+    'module_vendor' => 'Bnw',
+
+    'module_namespace' => 'Skeleton',
+
+    'module_tag' => 'skeleton',
 
 ];
